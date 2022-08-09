0c70ed0d0536f324d0ea7a3e42520d13f6d0a29b
sangeon22 <tkddjsdl33@hanmail.net>
2022-08-09 13:01:45 +0900

    출력문

---
+++ src/코드업/코드업기초1/Java1003.java
@@ -0,0 +1,7 @@
+package 코드업.코드업기초1;
+
+public class Java1003 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nWorld");
+    }
+}
---
+++ src/코드업/코드업기초1/Java1008.java
@@ -0,0 +1,10 @@
+package 코드업.코드업기초1;
+
+public class Java1008 {
+    public static void main(String[] args) {
+        System.out.print("\u250C\u252C\u2510\n");
+        System.out.print("\u251C\u253C\u2524\n");
+        System.out.print("\u2514\u2534\u2518");
+    }
+}
+
